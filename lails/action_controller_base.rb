259d1e6f3e7f8b0e8a3cc0a11d51495f38ad1604
Ayuto Takasaki <ayuto.takasaki@litalico.co.jp>
2019-04-22 10:59:40 +0900

    該当レイアウトの描画

--- lails/action_controller_base.rb
+++ lails/action_controller_base.rb
@@ -2,6 +2,7 @@
 
 require 'erb'
 require 'yaml'
+require 'cgi'
 
 module ActionController
 end
@@ -23,12 +24,15 @@ class ActionController::Base
   include ApplicationHelper
 
   def initialize
-    @views_root_path = "../../rails_tutorial/sample_app/app/views/"
-    @cookies = Cookies.new # 本当はもらってくる
+    @views_root_path = "../../rails_tutorial/sample_app/app/views/" # ToDo: 本当はどっかに書いておく
+    @cookies         = Cookies.new # 本当はもらってくる
   end
 
   def session
     {}
+  end
+
+  def provide(name_symbol, content)
   end
 
   def cookies
@@ -44,19 +48,19 @@ class ActionController::Base
   end
 
   def link_to(title, url, options = {})
-    "<A href='#{url}'>#{title}</A>"
+    "<A href='#{CGI.escape(url)}'>#{title}</A>"
   end
 
   def csrf_meta_tags
-
+    ''
   end
 
   def stylesheet_link_tag(path, options = {})
-
+    ''
   end
 
   def javascript_include_tag(path, options = {})
-
+    ''
   end
 
   def debug(target)
@@ -64,38 +68,53 @@ class ActionController::Base
   end
 
   def _invoke(method_symbol)
+    @render_state = nil
     # ここでメソッドの呼び出しをする
-    result = self.send(method_symbol)
-    _render_erb
+    self.send(method_symbol)
+    if @render_state == nil
+      # 特に指示がなければ、コントローラ・メソッドに対応するViewを探して描画する
+      controller_name  = self.class.name.gsub(/Controller$/, '').downcase
+      method_name      = method_symbol.to_s
+      target_filename  = "#{controller_name}/#{method_name}.html.erb"
+      current_rendered = _read_and_render_layout(@views_root_path + target_filename)
+      _read_and_render_erb current_rendered
+    else
+      fail "未実装"
+    end
   end
+
+  # ToDo: 動いているがこのへんごちゃついているのでリファクタする
 
   def render(target_name)
+    @render_state = '' # ToDo: その後の処理内容を記録する
     if target_name.include? "/"
-      _render_layout(File.dirname(target_name) + "/_" + File.basename(target_name) + ".html.erb")
+      _read_and_render_layout(File.dirname(target_name) + "/_" + File.basename(target_name) + ".html.erb")
     else
-
+      # 同じディレクトリ内なのでファイル名から即参照できる
     end
   end
 
-  def _render_layout(path)
-    erb_body = ""
-    File.open(path, "r") do |f|
-      erb_body = f.read
-    end
-    puts erb_body
+  def redirect_to(target)
+    @render_state = '' # ToDo: その後の処理内容を記録する
+  end
 
-    erb = ERB.new(erb_body)
+  def _read_and_render_layout(path, &block)
+    File.open(path, "r") do |f|
+      _render_erb(f.read, &block)
+    end
+  end
+
+  def _render_erb(source)
+    erb = ERB.new(source)
     erb.result(binding)
   end
 
-  def _render_erb
+  def _read_and_render_erb(current_rendered)
     # このブロック内全体で、ディレクトリの起点を変更しておく
     Dir.chdir @views_root_path do
-      # puts self.class.name
-
       # レイアウトファイル読み込んだりする
-      _render_layout "layouts/application.html.erb" do
-        "current file"
+      _read_and_render_layout "layouts/application.html.erb" do
+        current_rendered
       end
     end
   end
--- lails/rails.rb
+++ lails/rails.rb
@@ -7,31 +7,31 @@ end
 
 class Routes
   def get(path, options = {})
-    _define_path_helper(path)
+    _define_path_helper(path, path)
   end
 
   def root(path, options = {})
-    _define_path_helper('root')
+    _define_path_helper('root', '/')
   end
 
   def post(path, options = {})
-    _define_path_helper(path)
+    _define_path_helper(path, path)
   end
 
   def delete(path, options = {})
-    _define_path_helper(path)
+    _define_path_helper(path, path)
   end
 
   def resources(path, options = {})
-    _define_path_helper(path)
+    _define_path_helper(path, path)
   end
 
-  def _define_path_helper(path)
+  def _define_path_helper(path, url)
     method_name = (path.to_s).split("/").reject {|item| item == ""}.join("_") + "_path"
     puts "#{method_name} 自動登録"
     # 本当にこの実装でいいのか調べる
     Rails.define_method method_name.to_s do
-      "hogehoge"
+      url
     end
   end
 
