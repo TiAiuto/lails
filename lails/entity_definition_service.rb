17d1e82d3cb7b8a48602104f6041a2996f4bf650
Ayuto Takasaki <ayuto.takasaki@litalico.co.jp>
2019-04-26 17:27:38 +0900

    create_table, add_columnの実装

--- lails/entity_definition_service.rb
+++ lails/entity_definition_service.rb
@@ -1,3 +1,20 @@
+class ChangeMigrationBuilder
+  attr_reader :keys
+
+  def initialize
+    @keys = []
+  end
+
+  def string(name_symbol)
+    @keys << { name: name_symbol.to_s, type: :string }
+  end
+
+  def timestamps
+    string(:created_at)
+    string(:updated_at)
+  end
+end
+
 class EntityDefinitionService
   def initialize(db)
     @db = db
@@ -5,21 +22,40 @@ class EntityDefinitionService
 
   # ToDo: SQLインジェクション対策
 
-  def create_table(name, keys = [])
-    key_defs = keys.map do |item|
+  def create_table(name_symbol, &block)
+    change_builder = ChangeMigrationBuilder.new
+    block.call(change_builder)
+    keys = ["id INTEGER PRIMARY KEY"] + change_builder.keys.map do |item|
       "#{item[:name]} #{item[:type]}"
-    end.join(", ")
-    sql = <<END
-  CREATE TABLE #{name} (
-    #{key_defs}
+    end
+    sql  = <<END
+  CREATE TABLE #{name_symbol.to_s} (
+    #{keys.join(", ")}
   )
 END
     puts sql
     @db.execute sql
   end
 
-  def add_column
+  def create_table_without_id(name_symbol, &block)
+    change_builder = ChangeMigrationBuilder.new
+    block.call(change_builder)
+    keys = change_builder.keys.map do |item|
+      "#{item[:name]} #{item[:type]}"
+    end
+    sql  = <<END
+  CREATE TABLE #{name_symbol.to_s} (
+    #{keys.join(", ")}
+  )
+END
+    puts sql
+    @db.execute sql
+  end
 
+  def add_column(table_symbol, key_symbol, type_symbol)
+    sql = "ALTER TABLE #{table_symbol.to_s} ADD COLUMN #{key_symbol.to_s} #{type_symbol.to_s};"
+    puts sql
+    @db.execute sql
   end
 
   def add_index
--- lails/migrate.rb
+++ lails/migrate.rb
@@ -8,9 +8,9 @@ db          = SQLiteConnectionService.new(SQLITE_FILENAME).db
 def_service = EntityDefinitionService.new(db)
 
 unless def_service.table_exists? 'schema_migrations'
-  def_service.create_table 'schema_migrations', [
-    { name: 'version', type: 'string' }
-  ]
+  def_service.create_table :schema_migrations do |t|
+    t.string :version
+  end
 end
 
 version_max = db.execute('SELECT MAX(version) FROM schema_migrations')[0][0]
@@ -19,6 +19,15 @@ module ActiveRecord
 end
 
 class ActiveRecordMigration
+  attr_accessor :_def_service
+
+  def create_table(name_symbol, &block)
+    @_def_service.create_table(name_symbol, &block)
+  end
+
+  def add_column(table_symbol, key_symbol, type_symbol)
+    @_def_service.add_column(table_symbol, key_symbol, type_symbol)
+  end
 end
 
 class ActiveRecord::Migration
@@ -42,9 +51,9 @@ Dir.glob("#{APP_ROOT}db/migrate/*").sort.each do |filename|
   if can_process_migration
     classes_before = ActiveRecordMigration.subclasses.dup
     require filename
-    next_class = (ActiveRecordMigration.subclasses - classes_before).first
-    # next_class.new.change do
-    #
-    # end
+    next_class          = (ActiveRecordMigration.subclasses - classes_before).first
+    next_class_instance = next_class.new
+    next_class_instance._def_service = def_service
+    next_class_instance.change
   end
 end
