6a5d236582874dcbad11cf5ea061365c7ad6805c
Ayuto Takasaki <ayuto.takasaki@litalico.co.jp>
2019-04-26 14:22:59 +0900

    ファイル名修正

--- lails/config.rb
+++ lails/config.rb
@@ -4,7 +4,7 @@
 APP_ROOT = '../rails_tutorial/sample_app/'
 
 # コンパイル済みのSCSSファイルのファイル名
-COMPILED_CSS_FILENAME = 'tmp/custom.scss.css'
+COMPILED_CSS_FILENAME = 'custom.scss.css'
 
 # bootstrap.scssなどがあるフォルダ
 BOOTSTRAP_SASS_ROOT = '/Users/ayuto.takasaki/.rbenv/versions/2.5.1/lib/ruby/gems/2.5.0/gems/bootstrap-sass-3.3.7/'
