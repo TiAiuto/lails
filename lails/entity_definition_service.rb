64bff188299c48225fd9d361bdf69bc122a0994c
Ayuto Takasaki <ayuto.takasaki@litalico.co.jp>
2019-04-26 15:45:50 +0900

    i.f.変更

--- lails/entity_definition_service.rb
+++ lails/entity_definition_service.rb
@@ -3,8 +3,19 @@ class EntityDefinitionService
     @db = db
   end
 
-  def create_table
+  # ToDo: SQLインジェクション対策
 
+  def create_table(name, keys = [])
+    key_defs = keys.map do |item|
+      "#{item[:name]} #{item[:type]}"
+    end.join(", ")
+    sql = <<END
+  CREATE TABLE #{name} (
+    #{key_defs}
+  )
+END
+    puts sql
+    @db.execute sql
   end
 
   def add_column
@@ -17,5 +28,12 @@ class EntityDefinitionService
 
   def get_def
 
+  end
+
+  def table_exists?(name)
+    tables = @db.execute("SELECT * FROM sqlite_master")
+    !!tables.find do |row|
+      row[1] == "schema_migrations"
+    end
   end
 end
--- lails/migrate.rb
+++ lails/migrate.rb
@@ -3,20 +3,14 @@ require '../lails/entity_definition_service'
 require '../lails/config'
 
 # ここではmigrationのファイルを読み込んで順次適用していく
-db     = SQLiteConnectionService.new(SQLITE_FILENAME).db
-tables = db.execute("SELECT * FROM sqlite_master")
+db          = SQLiteConnectionService.new(SQLITE_FILENAME).db
+def_service = EntityDefinitionService.new(db)
 
-schema_table_exists = tables.find do |row|
-  row[1] == "schema_migrations"
-end
-
-unless schema_table_exists
-  sql = <<END
-  CREATE TABLE schema_migrations(
-    version text
-  )
-END
-  db.execute sql
+unless def_service.table_exists? 'schema_migr
+ations'
+  def_service.create_table 'schema_migrations', [
+    { name: 'version', type: 'string' }
+  ]
 end
 
 puts Dir.glob("#{APP_ROOT}db/migrate/*")
